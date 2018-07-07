d311ed8a9ff47512916f954fcd25f49d88b5555d
jpsiyu <jpsiyu@163.com>
2018-07-07 12:56:26 +0800

    chart

--- client/src/entry.jsx
+++ client/src/entry.jsx
@@ -16,7 +16,12 @@ class Entry extends React.Component {
             cashflow: null,
         }
         this.analysisState = [
-            { name: 'quote', open:false, callback: this.quoteAnalysis.bind(this), pass: false, },
+            { name: 'quote', path: 'quote', open: false, pass: false, },
+            { name: 'keyRatio', path: 'key_ratio', open: false, pass: false },
+            { name: 'income', path: 'income_statement', open: false, pass: false },
+            { name: 'balance', path: 'balance_sheet', open: false, pass: false },
+            { name: 'cashflow', path: 'cashflow', open: true, pass: false },
+        ]
         this.searchInput = null
         this.onBtnSearch = this.onBtnSearch.bind(this)
         this.url = pjson.runOnServer ? "http://120.78.240.132:3000" : "http://localhost"
@@ -37,64 +42,41 @@ class Entry extends React.Component {
             }
         }
         if (target) {
-            target.callback()
-        }else
+            this.specifyAnalysis(target)
+        } else
             console.log('Analysis Finish!')
     }
 
-    analysisPass(name){
-        for(let i = 0; i < this.analysisState.length; i++){
+    analysisPass(name) {
+        for (let i = 0; i < this.analysisState.length; i++) {
             let state = this.analysisState[i]
-            if(state.name == name){
+            if (state.name == name) {
                 state.pass = true
                 break
             }
         }
     }
 
-    quoteAnalysis() {
-        axios.get(`${this.url}/quote`).then(response => {
+    specifyAnalysis(state) {
+        axios.get(`${this.url}/${state.path}`).then(response => {
             const serverMsg = response.data
-            const quote = JSON.parse(serverMsg.msg)
-            this.setState({
-                quote: quote
-            })
-            this.analysisPass('quote')
+            const rawData = JSON.parse(serverMsg.msg)
+            const dictData = this.rawData2Dict(rawData)
+            const newState = {}
+            newState[state.name] = dictData
+            this.setState(newState)
+            this.analysisPass(state.name)
             this.analysisLoop()
         }).catch(error => console.log('ERR:', error))
     }
 
-    keyRatioAnalysis() {
-        axios.get(`${this.url}/key_ratio`).then(response => {
-            const serverMsg = response.data
-            const keyRatio = JSON.parse(serverMsg.msg)
-            this.setState({
-                keyRatio
-            })
-    }
-
-        axios.get(`${this.url}/income_statement`).then(response => {
-            const serverMsg = response.data
-            const income = JSON.parse(serverMsg.msg)
-            this.setState({
-                income
-            })
-    }
-
-        axios.get(`${this.url}/balance_sheet`).then(response => {
-            const serverMsg = response.data
-            const balance = JSON.parse(serverMsg.msg)
-            this.setState({
-                balance
-            })
-    }
-
-        axios.get(`${this.url}/cashflow`).then(response => {
-            const serverMsg = response.data
-            const cashflow = JSON.parse(serverMsg.msg)
-            this.setState({
-                cashflow
-            })
+    rawData2Dict(rawData){
+        let d = {}
+        for(let i = 0; i < rawData.length; i++){
+            const row = rawData[i]
+            d[row[0]] = row.slice(1)
+        }
+        return d
     }
 
     render() {
@@ -105,6 +87,7 @@ class Entry extends React.Component {
                         <div className='form-group form-inline'>
                             <input type='text' placeholder='Enter Ticker Symbol'
                                 className='form-control'
+                                ref={element => this.searchInput = element}
                                 required
                             />
                             <button type='submit' className='btn btn-primary'>Search</button>
@@ -113,16 +96,31 @@ class Entry extends React.Component {
                 </div>
             </div>
 
+            <ConditionTable title='Quote Info' data={this.state.quote} />
+            <ConditionTable title='Key Ratio' data={this.state.keyRatio} />
+            <ConditionTable title='Income Statement' data={this.state.income} />
+            <ConditionTable title='Balance Sheet' data={this.state.balance} />
+            <ConditionTable title='Cashflow' data={this.state.cashflow} />
+            <ConditionChart data={this.state.cashflow} />
         </div>
     }
 }
 
+const ConditionTable = (props) => {
+    if (tool.empty(props.data)) {
         return null
-    }else{
+    } else {
+        return <DataTable title={props.title} data={props.data} />
     }
 }
 
+const ConditionChart = (props) => {
+    if (tool.empty(props.data))
         return null
+    else {
+        const x = props.data['Fiscal year ends in December. CNY in millions except per share data.']
+        const y = props.data['Free cash flow']
+        return <Chart x={x} y={y} title='自由现金流' />
 
     }
 }
